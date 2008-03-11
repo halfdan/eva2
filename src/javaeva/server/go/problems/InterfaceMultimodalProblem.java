e26bbca3a812f36defc8459ba7c6926549dc7b89
Marcel Kronfeld <makron@gmx.net>
2008-03-11 11:08:17 +0000

    Missing interface.

---
+++ src/javaeva/server/go/problems/InterfaceMultimodalProblem.java
@@ -0,0 +1,10 @@
+package javaeva.server.go.problems;
+
+/**
+ * An interface to tag possibly multimodal problems, but without
+ * further knowledge about the optima.
+ * 
+ */
+public interface InterfaceMultimodalProblem {
+
+}
