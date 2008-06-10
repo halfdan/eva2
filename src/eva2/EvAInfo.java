763f2a45afe117194f32a8c83cd78030704e5095
Marcel Kronfeld <makron@gmx.net>
2008-06-10 09:08:55 +0000

    V. 2.022

--- src/eva2/EvAInfo.java
+++ src/eva2/EvAInfo.java
@@ -3,13 +3,16 @@ package eva2;
 /**
  * Main product and version information strings.
  * 
+ * 2.022: Some changes to the SimpleProblemWrapper, not of great interest. However,
+ * 		simple problems may now access a plot quite easily.
+ * 
  * @author mkron
  *
  */
 public class EvAInfo {
 	public static final String productName = "EvA 2";
 	public static final String productLongName = "Evolutionary Algorithms Workbench 2";
-	public static final String versionNum = new String ("2.021");
+	public static final String versionNum = new String ("2.022");
 	public static final String url = "http://www.ra.cs.uni-tuebingen.de/software/EvA2";
 
 	public static final String propertyFile = "resources/EvA2.props";
