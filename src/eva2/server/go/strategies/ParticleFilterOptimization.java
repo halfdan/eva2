b549822b2920ceca74e7a85e49016ebb9c2f3b2d
Marcel Kronfeld <makron@gmx.net>
2009-03-18 11:10:02 +0000

    Changes from MK rev. 255, update to ParticleFilter, and tool tips dont break at points.

--- src/eva2/server/go/strategies/ParticleFilterOptimization.java
+++ src/eva2/server/go/strategies/ParticleFilterOptimization.java
@@ -1,11 +1,14 @@
 package eva2.server.go.strategies;
 
 
+import eva2.gui.BeanInspector;
+import eva2.gui.GenericObjectEditor;
 import eva2.gui.Plot;
 import eva2.server.go.InterfacePopulationChangedEventListener;
 import eva2.server.go.individuals.AbstractEAIndividual;
 import eva2.server.go.individuals.InterfaceDataTypeDouble;
-import eva2.server.go.individuals.InterfaceESIndividual;
+import eva2.server.go.operators.distancemetric.EuclideanMetric;
+import eva2.server.go.operators.mutation.MutateESCorrVector;
 import eva2.server.go.operators.mutation.MutateESFixedStepSize;
 import eva2.server.go.operators.selection.InterfaceSelection;
 import eva2.server.go.operators.selection.SelectParticleWheel;
@@ -16,7 +19,8 @@ import eva2.server.go.problems.AbstractOptimizationProblem;
 import eva2.server.go.problems.F1Problem;
 import eva2.server.go.problems.InterfaceOptimizationProblem;
 
-/** This is a Particle Filter implemented by Frank Senke, only some documentation
+/**
+ * This is a Particle Filter implemented by Frank Senke, only some documentation
  * here and not completely checked whether this works on arbitrary problem
  * instances. MK did some adaptations, this should work on real valued problems now.
  * 
@@ -37,23 +41,37 @@ public class ParticleFilterOptimization implements InterfaceOptimizer, java.io.S
 	private static final long serialVersionUID = 1L;
 	private Population                      m_Population        = new Population();
     private InterfaceOptimizationProblem    m_Problem           = new F1Problem();
-    private InterfaceSelection              m_ParentSelection   = new SelectParticleWheel();
+    private InterfaceSelection              m_ParentSelection   = new SelectParticleWheel(0.5);
     //private boolean							m_UseElitism		= true;
 
     private String					m_Identifier = "";
     private boolean 				withShow = false;
-    private double					mutationSigma = 0.05;
+    private double					mutationSigma = 0.01;
+    private double					randomImmigrationQuota = 0.05;
+    private double					initialVelocity = 0.02;
+    private double					rotationDeg = 20.;
+    private int						popSize = 300;
 
     private int 					sleepTime = 0;
     
     transient private int indCount = 0;
     transient private InterfacePopulationChangedEventListener m_Listener;
     transient Plot myPlot = null;
+    public static final boolean TRACE = false;
 
     public ParticleFilterOptimization() {
         if (withShow) setWithShow(true);
     }
-
+    
+    public ParticleFilterOptimization(double vInit, double mute, double immiQuote, double rotDeg, double selScaling) {
+    	mutationSigma = mute;
+    	initialVelocity = vInit;
+    	randomImmigrationQuota = immiQuote;
+    	rotationDeg = rotDeg;
+    	m_ParentSelection = new SelectParticleWheel(selScaling);
+        if (withShow) setWithShow(true);
+    }
+    
     public ParticleFilterOptimization(ParticleFilterOptimization a) {
         this.m_Population                   = (Population)a.m_Population.clone();
         this.m_Problem                      = (InterfaceOptimizationProblem)a.m_Problem.clone();
@@ -65,19 +83,28 @@ public class ParticleFilterOptimization implements InterfaceOptimizer, java.io.S
         if (a.withShow) setWithShow(true);
     }
 
+    public void hideHideable() {
+    	GenericObjectEditor.setHideProperty(this.getClass(), "population", true);
+    }
+    
     public Object clone() {
         return (Object) new ParticleFilterOptimization(this);
     }
 
     public void init() {
-        this.m_Problem.initPopulation(this.m_Population);    	
         //System.out.println("popsize is   " + m_Population.size());
         //System.out.println("pops targ is " + m_Population.getPopulationSize());
-        for (int i=0; i<m_Population.size(); i++) {
-        	((AbstractEAIndividual)m_Population.getIndividual(i)).setMutationOperator(new MutateESFixedStepSize(mutationSigma));
-        }
-
+    	
+    	if (initialVelocity <= 0.) {
+    		(((AbstractOptimizationProblem)m_Problem).getIndividualTemplate()).setMutationOperator(new MutateESFixedStepSize(mutationSigma));
+    	} else {
+    		(((AbstractOptimizationProblem)m_Problem).getIndividualTemplate()).setMutationOperator(new MutateESCorrVector(mutationSigma, initialVelocity, rotationDeg));
+    	}
+    	m_Population.setPopSize(popSize);
+        this.m_Problem.initPopulation(this.m_Population);
+        
         setWithShow(withShow);
+
         this.evaluatePopulation(this.m_Population);  
         this.firePropertyChangedEvent(Population.nextGenerationPerformed);
     }
@@ -111,12 +138,37 @@ public class ParticleFilterOptimization implements InterfaceOptimizer, java.io.S
      */
     protected Population resample(Population pop) {
         Population parents;
+        boolean doImmigr=false;
         
         this.m_ParentSelection.prepareSelection(pop);
         
         // Generate a Population of Parents with Parantselectionmethod.
         // DONT forget cloning -> selection does only shallow copies!
-        parents = (Population)(this.m_ParentSelection.selectFrom(pop, this.m_Population.getPopulationSize())).clone();
+        int targetSize = this.m_Population.getPopulationSize();
+        if (randomImmigrationQuota>0) {
+        	if (randomImmigrationQuota>1.) System.err.println("Error, invalid immigration quota!");
+        	else {
+        		targetSize = (int)(this.m_Population.getPopulationSize() * (1.-randomImmigrationQuota));
+        		targetSize = Math.max(1, targetSize); // guarantee at least one to be selected 
+        		if (targetSize < this.m_Population.getPopulationSize()) doImmigr=true;
+        	}
+        }
+
+        parents = (Population)(this.m_ParentSelection.selectFrom(pop, targetSize)).clone();
+        
+        if (doImmigr) {
+        	// add immigrants
+    		AbstractEAIndividual immi;
+    		int i;
+        	for (i=0; (i+parents.getPopulationSize())<pop.getPopulationSize(); i++) {
+        		immi = (AbstractEAIndividual)pop.getEAIndividual(0).clone();
+        		immi.init(getProblem());
+        		parents.add(immi);
+        	}
+        	parents.synchSize();
+        	if (TRACE) System.out.println("Added " + i + " random individuals");
+        }
+
         parents.SetFunctionCalls(pop.getFunctionCalls());
         parents.setGenerationTo(pop.getGeneration());
         
@@ -146,7 +198,12 @@ public class ParticleFilterOptimization implements InterfaceOptimizer, java.io.S
     			if (useCircles) myPlot.getFunctionArea().drawCircle("", curPosition, graphLabel);
     			else myPlot.setUnconnectedPoint(curPosition[0], curPosition[1], graphLabel);
 //    			myPlot.setConnectedPoint(curPosition[0], curPosition[1], graphLabel);
-
+//    			if ( !useCircles && (pop.getEAIndividual(i).hasData(MutateESCorrVector.vectorKey))) {
+//    				double[] v=(double[])pop.getEAIndividual(i).getData(MutateESCorrVector.vectorKey);
+//    				myPlot.setConnectedPoint(curPosition[0], curPosition[1], graphLabel+5);
+//    				curPosition=Mathematics.vvAdd(v, curPosition);
+//    				myPlot.setConnectedPoint(curPosition[0], curPosition[1], graphLabel+5);
+//    			}
     		}
     	}
 	}
@@ -154,7 +211,7 @@ public class ParticleFilterOptimization implements InterfaceOptimizer, java.io.S
 	protected void applyMotionModel(AbstractEAIndividual indy, double noise) {
     	// this currently only performs a mutation
     	indy.mutate();
-    	indy.SetFitness(0, 0);
+    	indy.resetFitness(0);
     }
     
     /**
@@ -173,6 +230,7 @@ public class ParticleFilterOptimization implements InterfaceOptimizer, java.io.S
 
         // predict step
         predict(nextGeneration);
+        if (TRACE) System.out.println("Speed is  " + BeanInspector.toString(ParticleSwarmOptimization.getPopulationVelSpeed(m_Population, 3, MutateESCorrVector.vectorKey, null, null)) + " popM " + BeanInspector.toString(m_Population.getPopulationMeasures(new EuclideanMetric())));
         
         m_Population = evaluatePopulation(nextGeneration);
         
@@ -266,7 +324,6 @@ public class ParticleFilterOptimization implements InterfaceOptimizer, java.io.S
         return this.m_Population;
     }
     public void setPopulation(Population pop){
-//        if (pop.size()!=pop.getPopulationSize()) pop.fitToSize();
         this.m_Population = pop;
     }
     public String populationTipText() {
@@ -296,6 +353,10 @@ public class ParticleFilterOptimization implements InterfaceOptimizer, java.io.S
 		return withShow;
 	}
 
+	public Plot getPlot() {
+		return myPlot;
+	}
+	
 	protected void clearPlot() {
 		if (myPlot!=null) {
 			myPlot.clearAll();
@@ -356,6 +417,47 @@ public class ParticleFilterOptimization implements InterfaceOptimizer, java.io.S
 	}
 	
 	public String mutationSigmaTipText() {
-		return "The mutation step for the gaussian motion model";
+		return "The (fixed) mutation step for the gaussian motion model";
+	}
+
+	public double getRndImmigrQuota() {
+		return randomImmigrationQuota;
+	}
+
+	public void setRndImmigrQuota(double randomImmigrationQuota) {
+		this.randomImmigrationQuota = randomImmigrationQuota;
+	}
+	
+	public String rndImmigrQuotaTipText() {
+		return "The give ratio of the population will be reinitialized randomly in every iteration.";
+	}
+
+	public double getInitialVelocity() {
+		return initialVelocity;
+	}
+
+	public void setInitialVelocity(double initialVelocity) {
+		this.initialVelocity = initialVelocity;
+	}
+	
+	public String initialVelocityTipText() {
+		return "If > 0, a linear motion model will be applied, otherwise the gaussian model"; 
+	}
+
+	public double getRotationDeg() {
+		return rotationDeg;
+	}
+
+	public void setRotationDeg(double rotationDeg) {
+		this.rotationDeg = rotationDeg;
+	}
+
+	public int getPopSize() {
+		return popSize;
+	}
+
+	public void setPopSize(int popSize) {
+		this.popSize = popSize;
+		m_Population.setPopSize(popSize);
 	}
 }
