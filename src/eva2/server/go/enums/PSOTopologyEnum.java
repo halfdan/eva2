4ecb159d5e815dfd6c4df9ae7a2ec91ff73d1bec
Marcel Kronfeld <makron@gmx.net>
2009-05-04 15:49:31 +0000

    Improve handling of externally set initial population in PSO - minor change.

--- src/eva2/server/go/enums/PSOTopologyEnum.java
+++ src/eva2/server/go/enums/PSOTopologyEnum.java
@@ -1,50 +1,51 @@
 package eva2.server.go.enums;
 
 public enum PSOTopologyEnum {
-    /**
-    *
-    */
-   linear,
-   /**
-    *
-    */
-   grid,
-   /**
-    *
-    */
-   star,
-   /**
-    *
-    */
-   multiSwarm,
-   /**
-    *
-    */
-   tree,
-   /**
-    *
-    */
-   hpso,
-   /**
-    *
-    */
-   random; 
-   
-   /**
-    * A method to translate the "old" integer tags into the enum type.
-    * @param oldID
-    * @return
-    */
-   public static PSOTopologyEnum translateOldID(int oldID) {
-	   switch (oldID) {
-	   case 0: return linear;
-	   case 1: return grid;
-	   case 2: return star;
-	   case 3: return multiSwarm;
-	   case 4: return tree;
-	   case 5: return hpso;
-	   case 6: return random;
-	   }
-	   return random;
-   }
+	/**
+	 *
+	 */
+	linear,
+	/**
+	 *
+	 */
+	grid,
+	/**
+	 *
+	 */
+	star,
+	/**
+	 *
+	 */
+	multiSwarm,
+	/**
+	 *
+	 */
+	tree,
+	/**
+	 *
+	 */
+	hpso,
+	/**
+	 *
+	 */
+	random; 
+
+	/**
+	 * A method to translate the "old" integer tags into the enum type.
+	 * @param oldID
+	 * @return
+	 */
+	public static PSOTopologyEnum translateOldID(int oldID) {
+		switch (oldID) {
+		case 0: return linear;
+		case 1: return grid;
+		case 2: return star;
+		case 3: return multiSwarm;
+		case 4: return tree;
+		case 5: return hpso;
+		case 6: return random;
+		default:	System.err.println("Error: invalid old topology ID in PSOTopologyEnum translateOldID! Returning grid.");
+			return grid; 
+		}
+	}
 }
