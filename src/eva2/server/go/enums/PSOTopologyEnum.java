888684e2c29f4764aab19c253109225b5d90defb
Marcel Kronfeld <makron@gmx.net>
2009-05-04 15:45:52 +0000

    Improve handling of externally set initial population in PSO - adding a translation method.

--- src/eva2/server/go/enums/PSOTopologyEnum.java
+++ src/eva2/server/go/enums/PSOTopologyEnum.java
@@ -29,4 +29,22 @@ public enum PSOTopologyEnum {
     *
     */
    random; 
+   
+   /**
+    * A method to translate the "old" integer tags into the enum type.
+    * @param oldID
+    * @return
+    */
+   public static PSOTopologyEnum translateOldID(int oldID) {
+	   switch (oldID) {
+	   case 0: return linear;
+	   case 1: return grid;
+	   case 2: return star;
+	   case 3: return multiSwarm;
+	   case 4: return tree;
+	   case 5: return hpso;
+	   case 6: return random;
+	   }
+	   return random;
+   }
 }
