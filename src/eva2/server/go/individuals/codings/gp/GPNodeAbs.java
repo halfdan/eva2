ca490cf482b9fc6056ccbf6b5669bdac413f98dc
Marcel Kronfeld <makron@gmx.net>
2009-10-19 10:04:05 +0000

    New GP node and help-file for GenericConstraint.

---
+++ src/eva2/server/go/individuals/codings/gp/GPNodeAbs.java
@@ -0,0 +1,79 @@
+package eva2.server.go.individuals.codings.gp;
+
+import eva2.server.go.problems.InterfaceProgramProblem;
+
+
+/** 
+ * A node for retrieving the absolute value
+ * 
+ */
+public class GPNodeAbs extends AbstractGPNode implements java.io.Serializable {
+
+    public GPNodeAbs() {
+    }
+    public GPNodeAbs(GPNodeAbs node) {
+        this.m_Depth    = node.m_Depth;
+        this.m_Parent   = node.m_Parent;
+        this.m_Nodes    = new AbstractGPNode[node.m_Nodes.length];
+        for (int i = 0; i < node.m_Nodes.length; i++) this.m_Nodes[i] = (AbstractGPNode) node.m_Nodes[i].clone();
+    }
+
+    /** This method allows you to determine wehter or not two subtrees
+     * are actually the same.
+     * @param obj   The other subtree.
+     * @return boolean if equal true else false.
+     */
+    public boolean equals(Object obj) {
+        if (obj instanceof GPNodeAbs) {
+        	GPNodeAbs node = (GPNodeAbs)obj;
+            if (this.m_Nodes.length != node.m_Nodes.length) return false;
+            for (int i = 0; i < this.m_Nodes.length; i++) {
+                if (!this.m_Nodes[i].equals(node.m_Nodes[i])) return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** This method will be used to identify the node in the GPAreaEditor
+     * @return The name.
+     */
+    public String getName() {
+        return "Abs";
+    }
+
+    /** This method allows you to clone the Nodes
+     * @return the clone
+     */
+    public Object clone() {
+        return (Object) new GPNodeAbs(this);
+    }
+
+    /** This method will return the current arity
+     * @return Arity.
+     */
+    public int getArity() {
+        return 1;
+    }
+
+    /** This method will evaluate a given node
+     * @param environment
+     */
+    public Object evaluate(InterfaceProgramProblem environment) {
+        Object tmpObj;
+        double result = 0;
+
+        tmpObj = this.m_Nodes[0].evaluate(environment);
+        if (tmpObj instanceof Double) result += ((Double)tmpObj).doubleValue();
+        Double ret = new Double(result);
+        
+        if (ret<0) return -ret; 
+        else return ret;
+    }
+
+    @Override
+    public String getOpIdentifier() {
+    	return "abs";
+    }
+}
