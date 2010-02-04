18c772f61cc8597368dd85a8e5ff354cdf4f8f6f
Alexander Dörr <alexander.doerr@uni-tuebingen.de>
2010-02-04 15:46:17 +0000

    add Classes for event-driven DES

---
+++ src/eva2/tools/math/des/EventDESystem.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package eva2.tools.math.des;
+
+/**
+ * This Class represents an event-driven DES
+ * 
+ * @author <a href="mailto:a.doerr@uni-tuebingen.de">Alexander D&ouml;rr</a>
+ * @date 2010-02-04
+ *
+ */
+public interface EventDESystem extends DESystem {
+	
+	/**
+	 * Returns an array with delays (entries >=0) for the
+	 * events triggered either by the time t or by the concentrations
+	 * of the species stored in Y. The new values for the species
+	 * are stored in res. The positions in the array returned by this method
+	 * correspond to the positions in Y/res. 
+	 *
+	 * @return Returns an array with delays for the change of concentration due to events
+	 */
+	public double[] processEvents(double t, double Y[], double res[]);
+
+}
