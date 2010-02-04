b23f7ed6ca0d431a2c6364fa9973c2836e4e00e2
Alexander Dörr <alexander.doerr@uni-tuebingen.de>
2010-02-04 15:46:17 +0000

    add Classes for event-driven DES

---
+++ src/eva2/tools/math/des/AbstractDESSolver.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package eva2.tools.math.des;
+
+/**
+ * This Class represents an RKSolver for event-driven DES
+ * 
+ * @author <a href="mailto:a.doerr@uni-tuebingen.de">Alexander D&ouml;rr</a>
+ * @date 2010-02-04
+ *
+ */
+public abstract class AbstractDESSolver implements DESSolver {
+
+
+}
