537cee21fe69b4c9e974aa81adc6ccda9a026fba
Alexander Dörr <alexander.doerr@uni-tuebingen.de>
2010-02-11 16:28:22 +0000

    new class to support the processing of events

---
+++ src/eva2/tools/math/des/DESAssignment.java
@@ -0,0 +1,32 @@
+package eva2.tools.math.des;
+
+public class DESAssignment{
+	private double processTime;
+	private int index;
+	private Double value;
+	
+	public DESAssignment(double processTime, int index, Double value){
+		this.processTime = processTime;
+		this.index = index;
+		this.value = value;			
+	}
+	
+	public DESAssignment(double processTime, int index){
+		this.processTime = processTime;
+		this.index = index;
+		this.value = null;			
+	}
+	
+	public double getProcessTime() {
+		return processTime;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+	
+}
