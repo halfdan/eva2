65d22fb7dee5a7a1472ab5ff1e3f36cef58a6139
Alexander Dörr <alexander.doerr@uni-tuebingen.de>
2010-02-11 12:19:53 +0000

    fixed bugs with eventprocessing

--- src/eva2/tools/math/des/AbstractDESSolver.java
+++ src/eva2/tools/math/des/AbstractDESSolver.java
@@ -4,7 +4,7 @@
 package eva2.tools.math.des;
 
 /**
- * This Class represents an RKSolver for event-driven DES
+ * This Class represents an Solver for event-driven DES
  * 
  * @author <a href="mailto:a.doerr@uni-tuebingen.de">Alexander D&ouml;rr</a>
  * @date 2010-02-04
