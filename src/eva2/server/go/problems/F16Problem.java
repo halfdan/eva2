5cda401398717137c14b06c523dacc620c5a0217
Marcel Kronfeld <makron@gmx.net>
2010-02-24 10:46:14 +0000

    Merging old MK branch rev. 214: adding 4 multimodal problems

---
+++ src/eva2/server/go/problems/F16Problem.java
@@ -0,0 +1,55 @@
+package eva2.server.go.problems;
+
+public class F16Problem extends AbstractProblemDouble implements InterfaceMultimodalProblem, Interface2DBorderProblem {
+	int dim = 10;
+	
+	public F16Problem() {
+		dim=10;
+	}
+	
+	public F16Problem(F16Problem other) {
+		dim = other.dim;
+	}
+	
+	@Override
+	public double[] eval(double[] x) {
+		double[] res = new double[1];
+		double sum = 0;
+		
+		for (int i=0; i<getProblemDimension(); i++) {
+			sum += Math.sin(10*Math.log(x[i]));
+		}
+		
+		res[0] = 1.-((1./getProblemDimension())*sum);
+		return res;
+	}
+
+	@Override
+	public int getProblemDimension() {
+		return dim;
+	}
+	
+	public void setProblemDimension(int newDim) {
+		dim = newDim;
+	}
+
+	@Override
+	public Object clone() {
+		return new F16Problem(this);
+	}
+	
+	public double getRangeLowerBound(int n) {
+		return 0.25;
+	}
+	public double getRangeUpperBound(int n) {
+		return 10.;
+	}
+
+	public String getName() {
+		return "Vincent function";
+	}
+	
+	public String globalInfo() {
+		return "Multiple optima with increasing densitiy near the lower bounds, there for decreasing attractor size.";
+	}
+}
