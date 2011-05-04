8852b8c8e9643f553a81c6cc7fb6a82579298a0b
Marcel Kronfeld <makron@gmx.net>
2011-05-04 13:03:01 +0000

    Enums for statistics

---
+++ src/eva2/server/stat/StatsOnTwoSampledDataEnum.java
@@ -0,0 +1,12 @@
+package eva2.server.stat;
+
+public enum StatsOnTwoSampledDataEnum {
+	tTestEqLenEqVar, tTestUneqLenEqVar, tTestUneqLenUneqVar, mannWhitney;
+	
+	public static String[] getInfoStrings(){
+		return new String[] {"Two-sampled t-Test with equal sized data sets", 
+				"Two-sampled t-Test with unequal sized data sets", 
+				"Two-sampled t-Test with unequal data sets and unequal variances", 
+				"Two-sampled Mann-Whitney test"}; 
+	}
+}
