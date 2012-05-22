97ffdbf33ad2e42dd32ed0b5c8b38acaee77f0e7
Fabian Becker <fabian.becker@uni-tuebingen.de>
2012-05-22 13:52:39 +0000

    refs #17
    Added JavaHelp and some first entries into the documentation. JExtDesktopPane now defines keystrokes to access tiling/cascading windows. The main GUI has a new menu structure, showing a preferences entry in the options menu and a "Help Contents" under the Help menu.

---
+++ resources/EvA2Help/EvA2Help.hs
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?><!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<helpset version="2.0"><maps><mapref location="EvA2Help.jhm"/><homeID>html_index_html</homeID></maps><title>EvA2 Help</title><view><type>javax.help.TOCView</type><name>TOC</name><label>Table Of Contents</label><data>toc.xml</data></view><view><type>javax.help.SearchView</type><name>Search</name><label>Search</label><data>JavaHelpSearch</data></view></helpset>
