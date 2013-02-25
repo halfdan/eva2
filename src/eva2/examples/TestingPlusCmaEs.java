11005dd96b7c72bd147d6a75132b40698136050d
Fabian Becker <fabian.becker@uni-tuebingen.de>
2013-02-25 16:18:04 +0100

    Reformatted several files, code cleanup

--- src/eva2/examples/TestingPlusCmaEs.java
+++ src/eva2/examples/TestingPlusCmaEs.java
@@ -1,4 +1,5 @@
 package eva2.examples;
+
 import eva2.OptimizerFactory;
 import eva2.optimization.individuals.AbstractEAIndividual;
 import eva2.optimization.operators.crossover.CrossoverESDefault;
@@ -9,32 +10,34 @@ import eva2.optimization.strategies.EvolutionStrategies;
 import eva2.optimization.modules.GOParameters;
 
 public class TestingPlusCmaEs {
-	public static void main(String[] args) {
-		// a simple bimodal target function, two optima near (1.7,0) and (-1.44/0)
-		FM0Problem fm0 = new FM0Problem();              
-		AbstractEAIndividual bestIndy;          
-		// create standard ES parameters                
-		GOParameters esParams = OptimizerFactory.standardES(fm0);               
-		esParams.setTerminator(new EvaluationTerminator(2000));                 
-		// set a random seed based on system time               
-		esParams.setSeed(0);
 
-		// set evolutionary operators and probabilities                 
-		AbstractEAIndividual.setOperators(                              
-				fm0.getIndividualTemplate(),                            
-				new MutateESCovarianceMatrixAdaption(true), 0.9,                            
-				new CrossoverESDefault(), 0.1);         
+    public static void main(String[] args) {
+        // a simple bimodal target function, two optima near (1.7,0) and (-1.44/0)
+        FM0Problem fm0 = new FM0Problem();
+        AbstractEAIndividual bestIndy;
+        // create standard ES parameters                
+        GOParameters esParams = OptimizerFactory.standardES(fm0);
+        esParams.setTerminator(new EvaluationTerminator(2000));
+        // set a random seed based on system time               
+        esParams.setSeed(0);
 
-		// access the ES                
-		EvolutionStrategies es = (EvolutionStrategies)esParams.getOptimizer();          
-		// set a (1+5) selection strategy               
-		es.setMu(1);            
-		es.setLambda(5);                
-		es.setPlusStrategy(true);       
+        // set evolutionary operators and probabilities                 
+        AbstractEAIndividual.setOperators(
+                fm0.getIndividualTemplate(),
+                new MutateESCovarianceMatrixAdaption(true), 0.9,
+                new CrossoverESDefault(), 0.1);
 
-		// run optimization and retrieve winner individual              
-		bestIndy = (AbstractEAIndividual)OptimizerFactory.optimizeToInd(esParams, null);    
-		System.out.println(esParams.getTerminator().lastTerminationMessage() + "\nFound solution: " 
-				+ AbstractEAIndividual.getDefaultDataString(bestIndy));
-	};
+        // access the ES                
+        EvolutionStrategies es = (EvolutionStrategies) esParams.getOptimizer();
+        // set a (1+5) selection strategy               
+        es.setMu(1);
+        es.setLambda(5);
+        es.setPlusStrategy(true);
+
+        // run optimization and retrieve winner individual              
+        bestIndy = (AbstractEAIndividual) OptimizerFactory.optimizeToInd(esParams, null);
+        System.out.println(esParams.getTerminator().lastTerminationMessage() + "\nFound solution: "
+                + AbstractEAIndividual.getDefaultDataString(bestIndy));
+    }
+;
 }
