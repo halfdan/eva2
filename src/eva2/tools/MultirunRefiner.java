35044c2a1c294f756b98acc3f3257487092bf111
Fabian Becker <halfdan@xnorfz.de>
2013-10-12 19:38:34 +0200

    Added package-info.java to all packages Some more refactoring

--- src/eva2/tools/MultirunRefiner.java
+++ src/eva2/tools/MultirunRefiner.java
@@ -30,16 +30,16 @@ import java.util.Vector;
  */
 
 public class MultirunRefiner {
-    private JFrame m_Frame;
+    private JFrame mainFrame;
     private JPanel myPanel, myJButtonJPanel;
     private JButton refineJButton, exitJButton;
     //    private JButton				confidenceJButton;
-    private JTextArea m_InputText, m_OutputText;
+    private JTextArea inputText, outputText;
     private JScrollPane m_SP1, m_SP2;
-    private JMenuBar m_MenuBar;
-    private JMenu m_FileJMenu;
-    private JMenuItem m_LoadExpItem, m_SaveExpItem;
-    private JMenuItem m_ExitItem;
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem loadMenuItem, saveMenuItem;
+    private JMenuItem exitMenuItem;
 
     /**
      * Creates a new instance of MultirunRefiner
@@ -65,49 +65,49 @@ public class MultirunRefiner {
 
     public MultirunRefiner(String text, int numRuns) {
         starter();
-        m_InputText.setText(text);
+        inputText.setText(text);
     }
 
     public void starter() {
-        this.m_Frame = new JFrame("MultirunRefiner\u2122");
+        this.mainFrame = new JFrame("MultirunRefiner\u2122");
 
         // The menuebar
-        this.m_MenuBar = new JMenuBar();
-        this.m_FileJMenu = new JMenu("File");
-        this.m_LoadExpItem = new JMenuItem("Load");
-        this.m_LoadExpItem.setEnabled(true);
-        this.m_LoadExpItem.addActionListener(new ActionListener() {
+        this.menuBar = new JMenuBar();
+        this.fileMenu = new JMenu("File");
+        this.loadMenuItem = new JMenuItem("Load");
+        this.loadMenuItem.setEnabled(true);
+        this.loadMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ev) {
                 loadFile();
             }
         });
-        this.m_SaveExpItem = new JMenuItem("Save");
-        this.m_SaveExpItem.setEnabled(true);
-        this.m_SaveExpItem.addActionListener(new java.awt.event.ActionListener() {
+        this.saveMenuItem = new JMenuItem("Save");
+        this.saveMenuItem.setEnabled(true);
+        this.saveMenuItem.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 writeFile();
             }
         });
-        this.m_ExitItem = new JMenuItem("Exit");
-        this.m_ExitItem.setEnabled(true);
-        this.m_ExitItem.addActionListener(new java.awt.event.ActionListener() {
+        this.exitMenuItem = new JMenuItem("Exit");
+        this.exitMenuItem.setEnabled(true);
+        this.exitMenuItem.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 System.exit(0);
             }
         });
 
-        this.m_FileJMenu.add(this.m_LoadExpItem);
-        this.m_FileJMenu.add(this.m_SaveExpItem);
-        this.m_MenuBar.add(this.m_FileJMenu);
-        this.m_MenuBar.add(this.m_ExitItem);
-        this.m_Frame.setJMenuBar(this.m_MenuBar);
+        this.fileMenu.add(this.loadMenuItem);
+        this.fileMenu.add(this.saveMenuItem);
+        this.menuBar.add(this.fileMenu);
+        this.menuBar.add(this.exitMenuItem);
+        this.mainFrame.setJMenuBar(this.menuBar);
 
-        this.m_Frame.setSize(300, 300);
-        this.m_Frame.setLocation(0, 150);
-        this.m_Frame.addWindowListener(new WindowAdapter() {
+        this.mainFrame.setSize(300, 300);
+        this.mainFrame.setLocation(0, 150);
+        this.mainFrame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent ev) {
                 System.exit(0);
@@ -116,23 +116,23 @@ public class MultirunRefiner {
 
         this.myPanel = new JPanel();
         this.myPanel.setLayout(new GridLayout(1, 2));
-        this.m_InputText = new JTextArea();
-        this.m_OutputText = new JTextArea();
-        this.m_SP1 = new JScrollPane(this.m_InputText);
-        this.m_SP2 = new JScrollPane(this.m_OutputText);
+        this.inputText = new JTextArea();
+        this.outputText = new JTextArea();
+        this.m_SP1 = new JScrollPane(this.inputText);
+        this.m_SP2 = new JScrollPane(this.outputText);
         this.myPanel.add(this.m_SP1);
         this.myPanel.add(this.m_SP2);
-        this.m_Frame.getContentPane().add(this.myPanel, BorderLayout.CENTER);
+        this.mainFrame.getContentPane().add(this.myPanel, BorderLayout.CENTER);
 
         this.myJButtonJPanel = new JPanel();
         this.myJButtonJPanel.setLayout(new GridLayout(2, 2));
-        this.m_Frame.getContentPane().add(this.myJButtonJPanel, BorderLayout.SOUTH);
+        this.mainFrame.getContentPane().add(this.myJButtonJPanel, BorderLayout.SOUTH);
 
         refineJButton = new JButton("Refine Multiruns");
         refineJButton.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                m_OutputText.setText(refineToText(refine(m_InputText.getText())));
+                outputText.setText(refineToText(refine(inputText.getText())));
             }
         });
 //        confidenceJButton = new JButton("Create Matlab/Confidence");
@@ -152,8 +152,8 @@ public class MultirunRefiner {
 //        this.myJButtonJPanel.add(confidenceJButton);
         this.myJButtonJPanel.add(exitJButton);
 
-        m_Frame.validate();
-        m_Frame.setVisible(true);
+        mainFrame.validate();
+        mainFrame.setVisible(true);
     }
 
     /**
@@ -164,10 +164,10 @@ public class MultirunRefiner {
         JFileChooser fc = new JFileChooser();
         fc.setDialogTitle("Select an Multirun.TXT File");
         fc.setFileFilter(new TXTFileFilter());
-        int returnVal = fc.showDialog(this.m_Frame, "Load Multirun.TXT");
+        int returnVal = fc.showDialog(this.mainFrame, "Load Multirun.TXT");
         if (returnVal == 0) {
             readFile(fc.getSelectedFile());
-            this.m_Frame.validate();
+            this.mainFrame.validate();
         }
     }
 
@@ -176,7 +176,7 @@ public class MultirunRefiner {
         clearInputText();
         try {
             fileStream = new FileReader(f);
-            this.m_InputText.read(fileStream, f);
+            this.inputText.read(fileStream, f);
             return true;
         } catch (Exception e) {
             // TODO Auto-generated catch block
@@ -187,15 +187,15 @@ public class MultirunRefiner {
     }
 
     public void clearInputText() {
-        this.m_InputText.setText("");
+        this.inputText.setText("");
     }
 
     public void clearOutputText() {
-        this.m_OutputText.setText("");
+        this.outputText.setText("");
     }
 
     public void addOutputText(String t) {
-        this.m_OutputText.setText(this.m_OutputText.getText() + t);
+        this.outputText.setText(this.outputText.getText() + t);
     }
 
     /**
@@ -208,11 +208,11 @@ public class MultirunRefiner {
         JFileChooser fc = new JFileChooser();
         fc.setDialogTitle("Select destination file");
         fc.setFileFilter(new TXTFileFilter());
-        int returnVal = fc.showSaveDialog(this.m_Frame);
+        int returnVal = fc.showSaveDialog(this.mainFrame);
         if (returnVal == 0) {
             try {
                 FileWriter fileStream = new FileWriter(fc.getSelectedFile());
-                this.m_OutputText.write(fileStream);
+                this.outputText.write(fileStream);
             } catch (java.io.IOException ioe) {
             }
         }
@@ -319,11 +319,11 @@ public class MultirunRefiner {
 //        ArrayList   result = new ArrayList(), tmpA;
 //
 //        this.clearOutputText();
-//        for (int  i = 0; i < this.m_InputText.getLineCount(); i++) {
+//        for (int  i = 0; i < this.inputText.getLineCount(); i++) {
 //            try {
-//                begin = this.m_InputText.getLineStartOffset(i);
-//                end = this.m_InputText.getLineEndOffset(i);
-//                tmp = this.parseStringForDouble(this.m_InputText.getText(begin, end-begin));
+//                begin = this.inputText.getLineStartOffset(i);
+//                end = this.inputText.getLineEndOffset(i);
+//                tmp = this.parseStringForDouble(this.inputText.getText(begin, end-begin));
 //                if (tmp.length > 3) {
 //                    if (((int)(tmp[0])) == 1) numExp++;
 //                    if (result.size()-1 < ((int)(tmp[0]))) result.add(((int)(tmp[0])), new double[3]);
@@ -334,7 +334,7 @@ public class MultirunRefiner {
 //                }
 //            } catch (javax.swing.text.BadLocationException ble){}
 //        }
-//        System.out.println(this.m_InputText.getLineCount() + " lines parsed. " + numExp + " experiments with " + result.size() + " events each.");
+//        System.out.println(this.inputText.getLineCount() + " lines parsed. " + numExp + " experiments with " + result.size() + " events each.");
 //        this.addOutputText("Event\tBest\tMean\tWorst\n");
 //        for(int i = 0; i < result.size(); i++) {
 //            mean = ((double[])(result.get(i)));
@@ -345,7 +345,7 @@ public class MultirunRefiner {
     /**
      * A simple method to read doubles from a string.
      *
-     * @param String The string to be searched.
+     * @param searchme The string to be searched.
      * @return The array of doubles found.
      */
     public static double[] parseStringForDouble(String searchme) {
