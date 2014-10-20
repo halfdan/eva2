9dc1ebc27f38fb0abd854953826431ae7dc97ef9
Fabian Becker <halfdan@xnorfz.de>
2014-10-20 18:18:23 +0200

    Improve performance by limiting the amount of lines in the Optimization Log

--- src/eva2/gui/JTextEditorInternalFrame.java
+++ src/eva2/gui/JTextEditorInternalFrame.java
@@ -1,17 +1,4 @@
 package eva2.gui;
-/*
- * Title:        EvA2
- * Description:
- * Copyright:    Copyright (c) 2003
- * Company:      University of Tuebingen, Computer Architecture
- * @author Holger Ulmer, Felix Streichert, Hannes Planatscher
- * @version:  $Revision: 10 $
- *            $Date: 2006-01-18 11:02:22 +0100 (Wed, 18 Jan 2006) $
- *            $Author: streiche $
- */
-/*==========================================================================*
- * IMPORTS
- *==========================================================================*/
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
